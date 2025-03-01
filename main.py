390b6b5a84e76c71b2a7d3a55eebf3141d17c50c
Gabriel Souza <gabriel.ferreirat218@gmail.com>
2025-03-01 20:51:29 -0300

    Atualizando pela sexta vez

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ print("Hello, World!")
 
 print("Hello, World!")
 print("Primeira Modificação!")
+print("Segunda Modificação!")
+print("Terceira Modificação!")
+print("Quarta Modificação!")
+print("Quinta Modificação!")
+print("Sexta Modificação!")
